53266bdd587589eb40a88b47ea6487a613e043cc
kacper pirog <kacper.pirog@gmail.com>
2021-02-11 15:03:07 +0100

    add

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,26 +3,34 @@ import styles from "./List.scss";
 import Hero from "../Hero/Hero";
 import PropTypes from "prop-types";
 import Column from "../Column/Column";
+import { settings } from "../../data/dataStore";
 
 class List extends React.Component {
+  state = {
+    columns: this.props.columns || [],
+  };
   static propTypes = {
     title: PropTypes.node.isRequired,
-    children: PropTypes.node.isRequired,
+    description: PropTypes.node,
+    columns: PropTypes.array,
+    // children: PropTypes.node.isRequired,
     image: PropTypes.string,
-    column: PropTypes.array.isRequired,
   };
   static defaultProps = {
-    children: <p>I can do all the things!!!</p>,
+    description: settings.defaultListDescription,
   };
   render() {
     return (
       <section className={styles.component}>
         <Hero titleText={this.props.title} backgroundImage={this.props.image} />
-        <div className={styles.description}>{this.props.children}</div>
+        <div className={styles.description}>{this.props.description}</div>
         <div className={styles.columns}>
           <Column columnTitle={"Animals"}></Column>
           <Column columnTitle={"Plants"}></Column>
           <Column columnTitle={"Minerals"}></Column>
+          {/* {this.state.columns.map(({ key, ...columnProps }) => (
+            <Column key={key} {...columnProps} />
+          ))} */}
         </div>
       </section>
     );
