c9f5aface0d9df132e6bbf952db1f207aec87eff
kacper pirog <kacper.pirog@gmail.com>
2021-03-08 19:04:35 +0100

    add fix search

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -20,7 +20,7 @@ export const createAction_changeSearchString = (payload) => ({
 export default function reducer(statePart = "", action = {}) {
   switch (action.type) {
     case SEARCH:
-      return [...statePart, action.payload];
+      return action.payload;
     default:
       return statePart;
   }
