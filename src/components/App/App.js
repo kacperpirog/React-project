61e72ddb83cd18dc4d844569a25908360dbd74f0
kacper pirog <kacper.pirog@gmail.com>
2021-03-08 09:38:19 +0100

    add search

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import styles from './App.scss';
-
-// import List from '../List/List.js';
-import PropTypes from 'prop-types';
-import { pageContents, listData } from '../../data/dataStore';
-import ListContainer from '../List/ListContainer';
+import React from "react";
+import styles from "./App.scss";
+import PropTypes from "prop-types";
+import List from "../List/ListContainer.js";
+import Search from "../Search/SearchContainer.js";
+//import Card from '../Card/Card.js';
+//import Creator from '../Creator/Creator.js';
 
 class App extends React.Component {
+  state = {
+    cards: [],
+  };
+
   static propTypes = {
     title: PropTypes.node,
     subtitle: PropTypes.node,
     lists: PropTypes.array,
   };
+
+  addCard(title) {
+    this.setState((state) => ({
+      cards: [
+        ...state.cards,
+        {
+          key: state.cards.length
+            ? state.cards[state.cards.length - 1].key + 1
+            : 0,
+          title: title,
+        },
+      ],
+    }));
+  }
+
   render() {
     const { title, subtitle, lists } = this.props;
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
+        <Search />
         {lists.map((listData) => (
-          <ListContainer key={listData.id} {...listData} />
+          <List key={listData.id} {...listData} />
         ))}
       </main>
     );
