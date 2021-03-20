05f7ba987cb24c2568113ec9404c5b0bba1b5ac5
kacper pirog <kacper.pirog@gmail.com>
2021-03-20 23:16:15 +0100

    add dconteinr

---
+++ src/components/Container/Container.js
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+import React from "react";
+import styles from "./Container.scss";
+
+const Container = (props) => {
+  const { children } = props;
+
+  return <div className={styles.component}>{children}</div>;
+};
+
+Container.propTypes = {
+  children: PropTypes.any,
+};
+
+export default Container;
