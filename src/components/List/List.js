22842ddae052e12e59d4c426f778fe9239c42600
kacper pirog <kacper.pirog@gmail.com>
2021-03-22 11:30:51 +0100

    add container fix

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -24,21 +24,24 @@ class List extends React.Component {
   render() {
     const { title, image, description, columns, addColumn } = this.props;
     return (
-      <section className={styles.component}>
-        <Hero titleText={title} imageURL={image} />
-        <div className={styles.description}>{ReactHtmlParser(description)}</div>
+      <Container>
+        <section className={styles.component}>
+          <Hero titleText={title} imageURL={image} />
+          <div className={styles.description}>
+            {ReactHtmlParser(description)}
+          </div>
 
-        <div className={styles.columns}>
-          {columns.map((columnData) => (
-            <Column key={columnData.id} {...columnData} />
-          ))}
-        </div>
+          <div className={styles.columns}>
+            {columns.map((columnData) => (
+              <Column key={columnData.id} {...columnData} />
+            ))}
+          </div>
 
-        <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={addColumn} />
-        </div>
-        <Container />
-      </section>
+          <div className={styles.creator}>
+            <Creator text={settings.columnCreatorText} action={addColumn} />
+          </div>
+        </section>
+      </Container>
     );
   }
 }
